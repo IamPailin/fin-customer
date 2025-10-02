334506f562ec209994cd05b12fea6d26de951b83
IamPailin <pailindeelertpattana@gmail.com>
2025-10-02 11:52:15 +0700

    basepath added

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    instrumentationHook: true,
+    instrumentationHook: true, // only keep if you actually use instrumentation.js
   },
+  reactStrictMode: true,
+  swcMinify: true,
+  basePath: "/fin-customer", // 👈 important
+  assetPrefix: "/fin-customer/", // 👈 ensures JS/CSS chunks load correctly
 };
 
 export default nextConfig;
